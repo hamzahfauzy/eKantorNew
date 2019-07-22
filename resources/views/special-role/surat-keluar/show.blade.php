d26b87c22fab30b53ba9105e4423b0ac672daee6
Hamzah Fauzi <muhammadf@thebigrichgroup.com>
2019-07-22 12:34:00 +0700

    fix show surat keluar 2

--- resources/views/special-role/surat-keluar/show.blade.php
+++ resources/views/special-role/surat-keluar/show.blade.php
@@ -58,7 +58,7 @@
                                         @if(env('APP_ENV') == 'local')
                                         <iframe src="{{Storage::url($surat->file_surat_url)}}" style="width: 100%;height: 500px;" frameborder="0"></iframe>
                                         @else
-                                        <iframe src="http://docs.google.com/viewer?url={{url('/')}}{{Storage::url($surat->file_surat_url)}}&embedded=true" style="width: 100%;height: 500px;" frameborder="0"></iframe>
+                                        <iframe src="http://docs.google.com/viewer?url={{Storage::url($surat->file_surat_url)}}&embedded=true" style="width: 100%;height: 500px;" frameborder="0"></iframe>
                                         @endif
                                     </div>
                                 </div>
