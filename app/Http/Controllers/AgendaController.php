67648b1110df737cc1c552e50014cdab211617ce
Hamzah Fauzi <muhammadf@thebigrichgroup.com>
2019-09-16 15:57:04 +0700

    fix notification namespace

--- app/Http/Controllers/AgendaController.php
+++ app/Http/Controllers/AgendaController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Model\{Agenda,Setting};
+use App\Model\{Agenda,Setting,Notification};
 
 class AgendaController extends Controller
 {
