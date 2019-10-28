756ba2ff813ce74dc0a13dd4533fe44e17d7caa5
Hamzah Fauzi <muhammadf@thebigrichgroup.com>
2019-10-28 15:44:05 +0700

    spt model

--- app/Http/Controllers/SptController.php
+++ app/Http/Controllers/SptController.php
@@ -440,7 +440,7 @@ class SptController extends Controller
 			{
 				$sptEmployee = new SptEmployee;
 				$sptEmployee->create([
-					'spt_id' => $model->id,
+					'spt_id' => $request->id,
 					'employee_id' => $pengikut,
 				]);
 			}
